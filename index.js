4c0401885e7813e9673ebd30adb5d8aa4bc98a67
Potato <105754685+dragon731012@users.noreply.github.com>
2023-11-11 08:33:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const httpServer = http.createServer();
 
 const bareServer = createBareServer('/');
 
+
+var cron = require('node-cron');
+
+cron.schedule('*/15 * * * *', () => {
+  console.log('server pinged');
+});
+
+
 httpServer.on('request', (req, res) => {
 	if (bareServer.shouldRoute(req)) {
 		bareServer.routeRequest(req, res);
